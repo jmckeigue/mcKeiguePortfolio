a78a701de0989abbd5e45c0cdea0f939c5fc4aae
jmckeigue <mckeiguej24@hanover.edu>
2023-09-21 11:40:57 -0400

    more functionality to the hamburger menu

--- src/js/App.js
+++ src/js/App.js
@@ -9,3 +9,23 @@ scrollUp.addEventListener("click", () => {
         behavior: "smooth",
     })
 })
+
+// Nav hamburger
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+// Close hamburger when a link is clicked
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => 
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
