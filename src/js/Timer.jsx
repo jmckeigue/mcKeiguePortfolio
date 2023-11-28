a046479a919da5d8d37f10a75a29bd8607e8ee9e
jmckeigue <mckeiguej24@hanover.edu>
2023-11-28 11:31:25 -0500

    changed file type to .jsx

---
+++ src/js/Timer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'; 
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(30);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+          // Add code to display the popup warning here
+          alert("You've been on the website for 5 minutes!");
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000); 
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(myInterval);
+  }, [minutes, seconds]);
+
+  return (
+    <div>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default Timer;
