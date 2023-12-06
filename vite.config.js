6f1f93cc349519bee1d5e99198aefd673f2742d6
jmckeigue <mckeiguej24@hanover.edu>
2023-12-06 15:11:34 -0500

    Production edits

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from "path";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -9,6 +9,12 @@ export default defineConfig({
                 main:(__dirname, 'public/index.html')
             },
         },
+    },
+    resolve: {
+        alias: {
+            '/@/': path.resolve(__dirname, 'src/'),
+        },
+        extenstions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     },
     root: './',
     base: '/',
