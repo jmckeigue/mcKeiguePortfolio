e88bf49ac64bac0f1da727e9cbb50f722631113d
jmckeigue <mckeiguej24@hanover.edu>
2024-04-25 11:41:31 -0400

    Made changes to styling and resume on mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import ScrollToTopButton from '../components/ScrollToTopButton';
 import SocialSection from '../components/SocialSection';
 import { WindowLoad } from '../public/src/Functions'
-import '../styles/design_token.css';
 import '../styles/global.css';
 import '../styles/styles.css';
 
